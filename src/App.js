3aa9a2b7d180221a23413332de0010c177b2cd05
farimah <farimah.hsni@gmail.com>
2023-04-23 18:47:25 +0430

    add route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,39 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './css/main.css';
 
 import Header from './compoents/Header';
+import Details from './compoents/Details';
+import Category from './compoents/Category';
+import Poem from './compoents/Poem';
+import Form from './compoents/Form';
+import Home from './compoents/Home';
+
+
+import { Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
-     <div className='container-fluid'>
+<>
       <Header />
-     </div>
+      <main className='container'>
+          
+          {/* <Details/>
+           <Form/>  */}
+           
+      <Routes>
+
+          <Route path="/details/:id" element={<Details />} />
+          {/* <Route path="search/details/:id" element={<Details />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/poem/:id" element={<Poem />} />
+          <Route path="/login" element={<Form />} />
+          {/* <Route path="/search" element={<Cartsearch/>} />        */}
+      </Routes>
+      </main>
+
+
+
+</>
   );
 };
 
