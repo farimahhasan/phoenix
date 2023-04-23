2ad51d3ed3afbba7076268616d91d81ea5d62810
farimah <farimah.hsni@gmail.com>
2023-04-23 18:49:55 +0430

    add home component

---
+++ src/compoents/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from './Heading';
+import Slider from './Slider';
+
+const Home = () => {
+    return (
+        <>
+           <Heading />
+           <Slider /> 
+        </>
+    );
+};
+
+export default Home;
