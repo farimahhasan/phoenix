5d4cff83b164ab5fd3a505aa050cae6d3ab008af
farimah <farimah.hsni@gmail.com>
2023-04-23 18:49:34 +0430

    edit header component

--- src/compoents/Header.js
+++ src/compoents/Header.js
@@ -1,30 +1,47 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
+
+import home from '../images/home_icon.svg'
 import person_icon from '../images/person_icon.svg'
 import bag_icon from '../images/bag_icon.svg'
 import logo from '../images/logo.png'
 
+
 const Header = () => {
     return (
-        <header className='row align-items-center mt-2 '>
-                <div className='col-4 '>
-                    <nav>
-                        <ul className='m-0 p-0'>
-                            <li>
-                               <a href='#' className='text-decoration-none color_yellow'>صفحه اصلی</a>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary " dir='ltr'>
+                <div className="container">
+                    <Link className="navbar-brand" to={'/'}>
+                        <img src={logo} alt="logo" className='logo' />
+
+                    </Link>
+
+                    <div className="navbar-toggler menu btn15 " data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <div className="icon"></div>
+                        </div>
+                    <div className="collapse navbar-collapse  text-end mt-md-0 mt-sm-2 mt-2" id="navbarText">
+                        <span className="">
+                            <input id='search-input' type='text' placeholder='جستجو' dir='rtl' className='p-1 ' />
+                        </span>
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 mt-md-0 mt-sm-3 mt-3" dir='rtl'>
+                            <li className="nav-item ms-2">
+                                <img src={home} className='ms-2' />
+                                <Link to={'/'} className=' text-decoration-none color_yellow'>صفحه اصلی</Link>
+                            </li>
+                            <li className="nav-item mx-2">
+                            <img src={person_icon} alt="person icon" className='ms-2'/>
+                                <Link to={'/login'} className=' text-decoration-none color_yellow'>ورود کاربر</Link>                            </li>
+                            <li className="nav-item mx-2">
+                            <img src={bag_icon} className='ms-2' />
+                                <a href='#' className=' text-decoration-none color_yellow'>سبد خرید</a>
                             </li>
                         </ul>
-                    </nav>
-                </div>
-                <div className='col-4 text-center'>
-                  <h1 className='color_yellow m-0 p-0'>
-                   <img src={logo} alt="logo" className='logo' />
-                  </h1>
-                </div>
-                <div className='col-4 text-start'>
-                <img src={person_icon} alt="person icon" className='ms-1'/>
-                <img src={bag_icon} alt="bag icon" className='me-1'/>
 
+                    </div>
                 </div>
+            </nav>
+
         </header>
     );
 };
